dd1fff3b98d65b72e2cf791918157653c51397a9
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-10-28 22:17:34 +0300

    Update ContactForm.jsx

--- src/components/Phonebook/ContactForm/ContactForm.jsx
+++ src/components/Phonebook/ContactForm/ContactForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FormContainer, InputForm, Button } from './ContactFormStyled';
 import { useState } from 'react';
-import { PropTypes } from 'prop-types';
+// import { PropTypes } from 'prop-types';
 
 export const ContactForm = ({ onSubmit }) => {
   const [name, setName] = useState('');
